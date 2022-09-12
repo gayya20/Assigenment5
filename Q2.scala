6e7044689303f1390d3dee21a052577f5878e412
gayya20 <92713181+gayya20@users.noreply.github.com>
2022-09-12 22:15:48 +0530

    Update Q2.scala

--- Q2.scala
+++ Q2.scala
@@ -1,20 +1,8 @@
-object Q2 extends App {
-    val x = new Rational(3,4)
-    val y = new Rational(5,8)
-    val z = new Rational(2,7)
-  
-    val a = x-y-z
-    
-    println(a)
+object Q2 extends App{
+  //create objects
+  val r1 = new Rational(3,4)
+  val r2 = new Rational(5,8)
+  val r3 = new Rational(2,7)
+  println(" r1-r2-r3 : " + (r1-r2-r3))
 }
 
-class Rational(n:Int, d:Int) {
-    require(denom > 0, "Denominator must be greater than 0")
-    def numer = n
-    def denom = d
-    def neg = new Rational(-this.numer, this.denom)
-    def add(r:Rational) = new Rational(this.numer*r.denom + this.denom*r.numer, this.denom*r.denom)
-    def -(r:Rational) = this.add(r.neg)
-
-    override def toString(): String = numer + "/" + denom
-}
